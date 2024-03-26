4a495a9e34891a9c5721be81f8d1dc2fdd1edb01
Ahmed <musallam@adobe.com>
2024-03-26 18:01:56 -0500

    add helpers

--- index.js
+++ index.js
@@ -35,16 +35,13 @@ export const html2text = (html) => {
 };
 
 /**
- *
- * @param {*} text
+ * converts text with attributes to <span> elements wit given attributes.
+ * eg: [text]{color="red" class="highlight"} => <span color="red" class="highlight">text</span>
+ * @param {string} expression
  */
 export const text2html = (expression) => {
-  // Regular expression to match the pattern
   const regex = /\[([^[\]]*)\]\{(.*?)\}/g;
-
-  // Replace function
   return expression.replace(regex, function (match, text, attrs) {
-    // Encode the text and attribute values
     text = encodeHTML(text);
     attrs = attrs
       .split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/g) // match spaces only if not within quotes
@@ -58,3 +55,37 @@ export const text2html = (expression) => {
     return `<span ${attrs}>${text}</span>`;
   });
 };
+
+/**
+ * walkes the DOM and replaces text nodes with spans if they have inline-attributes
+ * @param {HTMLElement} main
+ */
+export function decorateSpans(main) {
+  const walker = document.createTreeWalker(
+    main,
+    NodeFilter.SHOW_TEXT,
+    null,
+    null
+  );
+  let node;
+  while ((node = walker.nextNode())) {
+    //console.log(node.textContent);
+    decorateTextNode(node);
+  }
+}
+
+/**
+ *
+ * @param {Node} node
+ */
+export function decorateTextNode(node) {
+  const textContent = node.textContent;
+
+  if (textContent.includes("[") && textContent.includes("]{")) {
+    const span = document.createElement("span");
+    span.innerHTML = text2html(textContent);
+    requestAnimationFrame(() => {
+      node.replaceWith(span);
+    });
+  }
+}
